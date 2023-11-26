c817f5a82112cc83ea3751164872c069f0d7800a
Yohan de Rose <yohan.derose@agoda.com>
2023-11-26 23:05:02 +0700

    feat: moving scheduler services into docker environment
    BREAKING CHANGE: first build is EXTREMELY slow

--- schedul_rs/src/lib.rs
+++ schedul_rs/src/lib.rs
@@ -5,6 +5,7 @@ pub mod utils {
     pub const KAFKA_TOPIC: &str = "job_replica";
     pub const LOCAL_KAFKA: &str = "localhost:29092";
     pub const DOCKER_KAFKA: &str = "job_replica_queue:9092";
+    pub const KAFKA_BROKER: &str = DOCKER_KAFKA;
 
     pub fn v1(path: &str) -> String {
         format!("/api/v1/{}", path)
@@ -20,7 +21,7 @@ pub mod leader {
 
     pub fn run() -> impl Future<Output = ()> {
         fn send_to_kafka(topic: &str, value: &str) -> Result<(), kafka::Error> {
-            let mut producer = Producer::from_hosts(vec![utils::LOCAL_KAFKA.to_owned()])
+            let mut producer = Producer::from_hosts(vec![utils::KAFKA_BROKER.to_owned()])
                 .with_ack_timeout(Duration::from_secs(1))
                 .with_required_acks(RequiredAcks::One)
                 .create()
@@ -36,6 +37,7 @@ pub mod leader {
         server.utilize(router! {
             get utils::v1("health") => |_req, _res| {
                 let buf = r#"{ "job_id" : "1234" }"#;
+                println!("job <- {}", buf);
                 let res = send_to_kafka(utils::KAFKA_TOPIC, buf);
                 match res {
                     Ok(_) => r#"{ "status" : "ok" }"#,
@@ -45,7 +47,7 @@ pub mod leader {
 
         });
 
-        server.listen("127.0.0.1:6767").unwrap();
+        server.listen("0.0.0.0:6767").unwrap();
         async {}
     }
 }
@@ -53,44 +55,52 @@ pub mod leader {
 pub mod follower {
     use crate::utils;
     use kafka::consumer::{Consumer, FetchOffset, GroupOffsetStorage};
+    use std::thread::sleep;
+    use std::time::Duration;
 
-    pub fn run() {
-        print!("Starting consumer... ");
-
-        let consumer = Consumer::from_hosts(vec![utils::LOCAL_KAFKA.to_owned()])
+    fn connect_to_kafka() -> Result<Consumer, kafka::Error> {
+        let consumer = Consumer::from_hosts(vec![utils::KAFKA_BROKER.to_owned()])
             .with_topic(utils::KAFKA_TOPIC.to_owned())
             .with_fallback_offset(FetchOffset::Earliest)
+            .with_group("job_replica_group".to_owned())
             .with_offset_storage(Some(GroupOffsetStorage::Kafka))
-            .with_group("job_replica".to_owned())
-            .create();
-        match consumer {
-            Ok(mut c) => {
-                println!("OK");
-                loop {
-                    let mss = c.poll().unwrap();
-                    for ms in mss.iter() {
-                        for m in ms.messages() {
-                            println!(
-                                "{}",
-                                std::str::from_utf8(m.value)
-                                    .unwrap_or("Couldn't decode job replica msg.")
-                            );
-                            // println!(
-                            //     "{}:{}@{}: {:?}",
-                            //     ms.topic(),
-                            //     ms.partition(),
-                            //     m.offset,
-                            //     m.value
-                            // );
-                        }
-                        c.consume_messageset(ms).unwrap();
-                    }
-                    c.commit_consumed().unwrap();
+            .create()?;
+        Ok(consumer)
+    }
+
+    pub fn run() {
+        print!("Starting consumer ... ");
+
+        let mut consumer: Option<Consumer> = None;
+        while consumer.is_none() {
+            consumer = match connect_to_kafka() {
+                Ok(c) => Some(c),
+                _ => None,
+            };
+            sleep(Duration::from_secs(3));
+        }
+
+        println!("OK");
+        let c = consumer.as_mut().unwrap();
+        loop {
+            let mss = c.poll().unwrap();
+            for ms in mss.iter() {
+                for m in ms.messages() {
+                    println!(
+                        "job -> {}",
+                        std::str::from_utf8(m.value).unwrap_or("Couldn't decode job replica msg.")
+                    );
+                    // println!(
+                    //     "{}:{}@{}: {:?}",
+                    //     ms.topic(),
+                    //     ms.partition(),
+                    //     m.offset,
+                    //     m.value
+                    // );
                 }
+                c.consume_messageset(ms).unwrap();
             }
-            Err(e) => {
-                println!("Error: {}", e);
-            }
+            c.commit_consumed().unwrap();
         }
     }
 }
--- schedul_rs/src/main.rs
+++ schedul_rs/src/main.rs
@@ -1,14 +1,21 @@
 use schedul_rs::follower;
 use schedul_rs::leader;
+use std::env;
 
 #[tokio::main]
 async fn main() {
-    let leader = tokio::spawn(async {
-        leader::run().await;
-    });
-    let follower = tokio::spawn(async {
-        follower::run();
-    });
+    match env::var("SCHED_TYPE") {
+        Ok(val) if val == "leader" => leader::run().await,
+        Ok(val) if val == "follower" => follower::run(),
+        _ => panic!("Invalid SCHED_TYPE env var. SCHED_TYPE must be either 'leader' or 'follower'"),
+    };
 
-    let _ = tokio::join!(leader, follower);
+    // Local Debugging
+    // let leader = tokio::spawn(async {
+    //     leader::run().await;
+    // });
+    // let follower = tokio::spawn(async {
+    //     follower::run();
+    // });
+    // let _ = tokio::join!(leader, follower);
 }
